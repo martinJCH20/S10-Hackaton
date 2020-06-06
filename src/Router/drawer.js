91168a81f66718345a6b107576f7ea04b0e09819
martinJCH20 <61629740+martinJCH20@users.noreply.github.com>
2020-06-06 14:27:57 -0500

    Add files via upload

---
+++ src/Router/drawer.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { MenuDrawer } from '../Components/Menu/MenuDrawer'
+import { MainStackNavigator } from './index'
+import Home from '../Scenes/Home';
+import Details from '../Scenes/Details';
+import Cart from '../Scenes/Cart';
+import Contact from '../Scenes/Contact';
+
+const Drawer = createDrawerNavigator();
+
+const DrawerStackNavigator = () => {
+   // console.warn('props', props)
+    return(
+        <Drawer.Navigator
+        initialRouteName={'Home'}
+        drawerContent={(props) => <MenuDrawer {...props}/>}
+        >
+            <Drawer.Screen
+                name='Home' 
+                component={Home}
+            />
+            <Drawer.Screen
+                name='Details' 
+                component={Details}
+            />
+            <Drawer.Screen
+                name='Cart' 
+                component={Cart}
+            />
+            <Drawer.Screen
+                name='Contact' 
+                component={Contact}
+            />              
+        </Drawer.Navigator>
+    )
+}
+
+export {
+DrawerStackNavigator
+}
